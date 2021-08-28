dd2a618e879e5674d6932db3b837365e3abdca83
sowish23 <soso2266@naver.com>
2021-08-29 01:30:01 +0900

    add card

--- src/App.ts
+++ src/App.ts
@@ -26,6 +26,7 @@ class App {
             })
         })
 
+        //signup
         this.application.post('/user', (req: express.Request, res: express.Response) => {
             const {id, password, name} = req.body;
             interface userSchema {
@@ -58,6 +59,7 @@ class App {
             })
         })
 
+        //login
         this.application.post('/login', (req: express.Request, res: express.Response) => {
             const {id, password} = req.body;
             interface loginSchema {
@@ -82,6 +84,7 @@ class App {
             })
         })
 
+        //list 추가
         this.application.post('/list', (req: express.Request, res: express.Response) => {
             const {userid, title} = req.body;
             interface listSchema {
@@ -107,6 +110,40 @@ class App {
                 })
                 .catch((err: any) => {
                     console.error(`Failed to insert list. id: ${err}`)
+                    res.send('fail')
+                })
+        })
+
+        //card 추가
+        this.application.post('/card', (req: express.Request, res: express.Response) => {
+            var ObjectId = require('mongodb').ObjectId
+            const {list_id, title, description} = req.body;
+            interface cardSchema {
+                title: string,
+                description: string,
+            }
+            let card: cardSchema;
+            card ={
+                title,
+                description
+            }
+            var ObjectId = require('mongodb').ObjectId
+            Connection.collection('card').insertOne(card)
+                .then((result: any) => {
+                    var card_id = result.insertedId
+                    const updateList = Connection.collection('list').findOneAndUpdate({"_id": ObjectId(list_id)},{"$push" : {card_id}})
+                    updateList.then((doc: any) => {
+                        if(!doc) {
+                            res.send('list id를 확인하세요')
+                        }
+                        else {
+                            console.log(`Success! id: ${result.insertedId}`)
+                            res.send('success')
+                        }
+                    })
+                })
+                .catch((err: any) => {
+                    console.error(`Failed to insert card. id: ${err}`)
                     res.send('fail')
                 })
         })
