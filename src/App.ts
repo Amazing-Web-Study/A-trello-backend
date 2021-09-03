9f00dda0ded81bc1e9790c1402bed1eab9cdcb4d
HO22 <flsghl@naver.com>
2021-09-03 22:14:54 +0900

    card, list delete 추가
    cardlist default 값 설정

--- src/App.ts
+++ src/App.ts
@@ -82,9 +82,20 @@ class App {
         this.application.post('/list/', (req: express.Request, res: express.Response) => {
             const insertCard = {
                 'title': req.body.title,
-                'cardList': req.body.cardList
+                'cardList': []
             }
             Connection.collection('cardList').insertOne(insertCard).then((createDocument: any) => {
+                res.send(createDocument)
+            }).catch((err: any) => {
+                res.send(err)
+            })
+        })
+        this.application.delete('/list/:listId/', (req: express.Request, res: express.Response) => {
+            const query = { _id: new ObjectId(req.params.listId) }
+            const options = {
+                justOne: true,
+            }
+            Connection.collection('cardList').remove(query, options).then((createDocument: any) => {
                 res.send(createDocument)
             }).catch((err: any) => {
                 res.send(err)
@@ -95,20 +106,36 @@ class App {
             const insertCard = {
                 "$push": {
                     "cardList": {
+                        "_id": new ObjectId(),
                         "title": req.body.title,
                         "description": req.body.description
                     }
                 }
             }
-            Connection.collection('cardList').find(query).toArray(function (err: any, result: any) {
-                if (err) throw err;
-                console.log(result)
-            })
             const options = {returnNewDocument: true};
             Connection.collection('cardList').findOneAndUpdate(query, insertCard, options).then((updateDocument: any) => {
-                console.log(updateDocument)
+                console.log('aaaa')
                 res.send(updateDocument)
             }).catch((err: any) => {
+                console.log(err)
+                res.status(500)
+                res.send(err)
+            })
+        })
+        this.application.delete('/list/:listId/:cardId', (req: express.Request, res: express.Response) => {
+            const query = { _id: new ObjectId(req.params.listId) }
+            const upsetCard = {
+                "$pull": {
+                    "cardList": {
+                        "_id": new ObjectId(req.params.cardId)
+                    }
+                }
+            }
+            const options = {returnNewDocument: true};
+            Connection.collection('cardList').findOneAndUpdate(query, upsetCard, options).then((deleteCard: any) => {
+                res.send(deleteCard)
+            }).catch((err: any) => {
+                res.status(500)
                 res.send(err)
             })
         })
